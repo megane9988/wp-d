1a6b370c7dfcaa4d65b6cf0c625b9935416a03f2
megane9988 <info@m-g-n.me>
2014-02-19 00:23:32 +0900

    #34 狭い画面幅での投稿者の表示を修正

--- index.php
+++ index.php
@@ -22,12 +22,18 @@
 		<?php endif; ?>
 		<?php while ( have_posts() ) : the_post(); ?>
 		<div class="row">
-			<div class="large-2 columns mb">
-				<?php
-					$author_bio_avatar_size = apply_filters( 'wp-d_author_bio_avatar_size', 320 );
-					echo get_avatar( get_the_author_meta( 'user_email' ), $author_bio_avatar_size );
-				?>
-				<p><a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author"><?php the_author(); ?></a></p>
+			<div class="large-2 columns">
+				<div class="row">
+					<div class="large-12 small-3 columns">
+						<?php
+							$author_bio_avatar_size = apply_filters( 'wp-d_author_bio_avatar_size', 320 );
+							echo get_avatar( get_the_author_meta( 'user_email' ), $author_bio_avatar_size );
+						?>
+					</div>
+					<div class="large-12 small-9 columns">
+						<p><a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author"><?php the_author(); ?></a></p>
+					</div>
+				</div>
 			</div>
 			<div class="large-10 columns">
 				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
