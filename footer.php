f571cdada9945ec259ec20148194f80cfca8a702
megane9988 <info@m-g-n.me>
2014-04-29 17:41:37 +0900

    giploytest 4

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@
 			<div class="row">
 				<div class="large-12 columns text-center">
 					<p>
-						Proudly powered by WordPresssss | Theme: <?php $wp_d_theme = wp_get_theme(); echo $wp_d_theme->get( 'Name' ); ?> by <?php echo $wp_d_theme->get( 'Author' ); ?>.
+						Proudly powered by WordPress | Theme: <?php $wp_d_theme = wp_get_theme(); echo $wp_d_theme->get( 'Name' ); ?> by <?php echo $wp_d_theme->get( 'Author' ); ?>.
 					</p>
 				</div>
 			</div>
